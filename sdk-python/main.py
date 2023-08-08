1e0d272cb721d74779f1c47324f6f16fd6261b86
Justin <57725347+juskek@users.noreply.github.com>
2023-08-08 17:47:47 +0100

    refactor: remove schema

--- sdk-python/main.py
+++ sdk-python/main.py
@@ -6,14 +6,10 @@ from database.write_data import write_data
 
 from models.get_llm_response import get_llm_response
 
-from database.write_schema import write_schema
-
 prompt = "What is a proompt?"
 response = get_llm_response(prompt)
 
 connection, cursor = connect_to_db()
-
-write_schema(connection, cursor)
 
 write_data(cursor, response.created, prompt, response.choices[0].text, response.usage.total_tokens)
 
--- sdk-python/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE llm_logs (
-    id SERIAL PRIMARY KEY,
-    unixtime_seconds BIGINT NOT NULL,
-    input_string TEXT NOT NULL,
-    output_string TEXT NOT NULL,
-    total_tokens INTEGER NOT NULL
-);
