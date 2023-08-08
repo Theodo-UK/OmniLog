3998c1b6fd113ec30bf17785691a1def2a8de633
Justin <57725347+juskek@users.noreply.github.com>
2023-08-08 18:02:17 +0100

    refactor: change unixtime to UTC datetime

--- sdk-python/database/write_data.py
+++ sdk-python/database/write_data.py
@@ -1,5 +1,5 @@
 def write_data(cur, time, prompt, response, tokens):
     cur.execute("""
-    INSERT INTO llm_logs (unixtime_seconds, input_string, output_string, total_tokens)
+    INSERT INTO llm_logs (datetime_utc, input_string, output_string, total_tokens)
     VALUES (%s, %s, %s, %s);
     """, (time, prompt, response, tokens))
