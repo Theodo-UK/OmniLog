c08e4abc2ce9320ac7bc632e240e67d6eeedef0c
Justin <57725347+juskek@users.noreply.github.com>
2023-08-08 16:00:24 +0100

    build: add env variables

--- sdk-python/main.py
+++ sdk-python/main.py
@@ -1,4 +1,9 @@
+import os
 import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def generate_prompt(animal: str):
     return f"""Suggest three names for an animal that is a superhero.
 
@@ -9,9 +14,15 @@ Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
 Animal: {animal.capitalize()}
 Names:"""
 
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if not OPENAI_API_KEY:
+    raise EnvironmentError(f"OPENAI_API_KEY not found: {OPENAI_API_KEY}\nMake sure that you have an OPENAI_API_KEY value in the .env file.")
+
+openai.api_key = OPENAI_API_KEY
+
 response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt("Mouse"),
             temperature=0.6,
         )
-
