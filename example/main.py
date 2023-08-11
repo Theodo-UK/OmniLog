cc740abb732900b48fb08fe8b58bec58bfd65e82
François Lorne <francois.lorne@hotmail.com>
2023-08-11 11:44:30 +0100

    feat: extract main to an example folder

--- example/main.py
+++ example/main.py
@@ -1,14 +1,16 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()
-from database.connect import connect_to_db
-from database.write_data import write_data
-from models.get_llm_response import get_llm_response
-from datetime import datetime 
+from omnilog.get_llm_response import get_llm_response
+from omnilog.write_data import write_data
+from omnilog.connect_to_db import connect_to_db
+from datetime import datetime
 
 prompt = "What is a proompt?"
 response = get_llm_response(prompt)
 
-connection, cursor = connect_to_db()
+DATABASE_URL = os.getenv("DATABASE_URL")
+connection, cursor = connect_to_db(DATABASE_URL)
 
 write_data(cursor, datetime.utcfromtimestamp(response.created), prompt, response.choices[0].text, response.usage.total_tokens)
 
