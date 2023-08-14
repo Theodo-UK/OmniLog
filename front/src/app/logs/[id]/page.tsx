ae436e9cfa2c768c3a5ef1fc23d94cd80f0309ca
Justin <57725347+juskek@users.noreply.github.com>
2023-08-14 11:44:31 +0100

    feat: create LogDetails page

---
+++ front/src/app/logs/[id]/page.tsx
@@ -0,0 +1,44 @@
+import { LogsData } from "@/data/LogsData";
+
+export default async function LogDetails({
+    params,
+}: {
+    params: { id: string };
+}) {
+    const logDetails = await LogsData.getLogDetails(params.id);
+
+    return (
+        <div className="border bg-white shadow rounded-md p-4 w-full mx-auto">
+            <table className="table-auto w-full">
+                <tbody>
+                    <tr>
+                        <td className="font-bold px-4 py-2">ID:</td>
+                        <td className="px-4 py-2">{logDetails.id}</td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold px-4 py-2">
+                            Date/Time (UTC):
+                        </td>
+                        <td className="px-4 py-2">
+                            {new Date(logDetails.datetime_utc).toLocaleString()}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold px-4 py-2">Input String:</td>
+                        <td className="px-4 py-2">{logDetails.input_string}</td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold px-4 py-2">Output String:</td>
+                        <td className="px-4 py-2">
+                            {logDetails.output_string}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="font-bold px-4 py-2">Total Tokens:</td>
+                        <td className="px-4 py-2">{logDetails.total_tokens}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
