1591a60067a84cf961a5a6c84bb97cf2e3588f84
François Lorne <francois.lorne@hotmail.com>
2023-08-15 10:43:44 +0100

    refactor: format sdk with black

--- sdk-python/src/omnilog/send_to_db.py
+++ sdk-python/src/omnilog/send_to_db.py
@@ -1,12 +1,16 @@
 import psycopg2
 
+
 def send_to_db(url, log):
     connection, cursor = connect_to_db(url)
 
-    cursor.execute("""
-    INSERT INTO llm_logs (datetime_utc, input_string, output_string, total_tokens)
+    cursor.execute(
+        """
+    INSERT INTO llm_logs (datetime_utc, input_string, output_string, total_tokens) this is a test for extra long string
     VALUES (%s, %s, %s, %s);
-    """, (log["datetime_utc"], log["input"], log["output"], log["total_tokens"]))
+    """,
+        (log["datetime_utc"], log["input"], log["output"], log["total_tokens"]),
+    )
 
     connection.commit()
     cursor.close()
@@ -21,15 +25,15 @@ def connect_to_db(url):
         raise
 
     cur = conn.cursor()
-    cur.execute('SELECT NOW();')
+    cur.execute("SELECT NOW();")
     time = cur.fetchone()[0]
 
-    cur.execute('SELECT version();')
+    cur.execute("SELECT version();")
     version = cur.fetchone()[0]
 
     if not time or not version:
         raise ConnectionError(f"Could not connect to database: {url}")
 
-    print('Current time:', time)
-    print('PostgreSQL version:', version)
+    print("Current time:", time)
+    print("PostgreSQL version:", version)
     return conn, cur
