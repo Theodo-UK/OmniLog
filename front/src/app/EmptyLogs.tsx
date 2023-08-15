ab6051f47c6b96bfa743d729c0d87d4d064286da
CallumHemsley <bythepenguin@gmail.com>
2023-08-15 10:21:55 +0100

    feat: add empty logs view

---
+++ front/src/app/EmptyLogs.tsx
@@ -0,0 +1,10 @@
+import { CardAtom } from "@/atomic/atoms/CardAtom";
+import Link from "next/link";
+
+export default function EmptyLogs() {
+    return (
+        <CardAtom>
+            <p className="px-4 py-2 text-gray-800 text-center">There are no logs yet. Please connect our SDK and start logging!  <Link className="underline" href="https://github.com/Theodo-UK/OmniLog/blob/main/README.md#quickstart">For more info, see here</Link>. </p>
+        </CardAtom>
+    );
+}
