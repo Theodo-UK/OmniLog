e47e2cbc5873d83220541ae7862ff6a7316097c8
François Lorne <francois.lorne@hotmail.com>
2023-08-18 10:57:14 +0100

    test: clean test file to fit function names

--- sdk-python/test/check_log_test.py
+++ sdk-python/test/check_log_test.py
@@ -1,11 +1,10 @@
-import psycopg2
-from omnilog.send_to_db import send_to_db
+from omnilog.send_to_db import check_log_type
 import unittest
 import datetime
 
 
-class TestTypes(unittest.TestCase):
-    valid_url = "mongodb://localhost:27017/"
+class TestLogType(unittest.TestCase):
+
     valid_log = {
         "datetime_utc": datetime.datetime.utcnow(),
         "input": "What is this?",
@@ -13,44 +12,25 @@ class TestTypes(unittest.TestCase):
         "total_tokens": 5,
     }
 
-    def test_valid_inputs(self):
+    def test_valid_log(self):
         """
-        When correct arguments are given, then connection with psycopg2 is tried and fails
+        When a correct log is given, then no error is raised
         """
-        self.assertRaises(
-            psycopg2.ProgrammingError,
-            send_to_db,
-            self.valid_url,
-            self.valid_log,
-        )
-
-    def test_url(self):
-        """
-        When url is not a string, then TypeError is raised
-        """
-        url = True
-        log = self.valid_log
-
-        self.assertRaisesRegex(
-            TypeError,
-            "Have you checked your url connection string is correct?",
-            send_to_db,
-            url,
-            log,
-        )
+        try:
+            check_log_type(self.valid_log)
+        except Exception:
+            self.fail("log checker raised an error unexpectedly!")
 
     def test_log_type(self):
         """
         When log is not a dictionary, then TypeError is raised
         """
-        url = self.valid_url
         log = True
 
         self.assertRaisesRegex(
             TypeError,
-            "Have you checked your log is a dictionary\?\nlog must be a dictionary with keys: datetime_utc, input, output, total_tokens",
-            send_to_db,
-            url,
+            r"Have you checked your log is a dictionary\?\nlog must be a dictionary with keys: datetime_utc, input, output, total_tokens",
+            check_log_type,
             log,
         )
 
@@ -58,14 +38,12 @@ class TestTypes(unittest.TestCase):
         """
         When log is missing a field, then KeyError is raised
         """
-        url = self.valid_url
         log = {"datetime_utc": datetime.datetime.utcnow()}
 
         self.assertRaisesRegex(
             KeyError,
             "log must be a dictionary with keys: datetime_utc, input, output, total_tokens",
-            send_to_db,
-            url,
+            check_log_type,
             log,
         )
 
@@ -73,15 +51,13 @@ class TestTypes(unittest.TestCase):
         """
         When log datetime_utc is not a datetime, then TypeError is raised
         """
-        url = self.valid_url
         log = self.valid_log.copy()
         log["datetime_utc"] = True
 
         self.assertRaisesRegex(
             TypeError,
             "log.datetime_utc must be a datetime object",
-            send_to_db,
-            url,
+            check_log_type,
             log,
         )
 
@@ -89,15 +65,13 @@ class TestTypes(unittest.TestCase):
         """
         When log input is not a string, then TypeError is raised
         """
-        url = self.valid_url
         log = self.valid_log.copy()
         log["input"] = True
 
         self.assertRaisesRegex(
             TypeError,
             "log.input must be a string",
-            send_to_db,
-            url,
+            check_log_type,
             log,
         )
 
@@ -105,15 +79,13 @@ class TestTypes(unittest.TestCase):
         """
         When log output is not a string, then TypeError is raised
         """
-        url = self.valid_url
         log = self.valid_log.copy()
         log["output"] = True
 
         self.assertRaisesRegex(
             TypeError,
             "log.output must be a string",
-            send_to_db,
-            url,
+            check_log_type,
             log,
         )
 
@@ -121,15 +93,13 @@ class TestTypes(unittest.TestCase):
         """
         When log total_tokens is not an integer, then TypeError is raised
         """
-        url = self.valid_url
         log = self.valid_log.copy()
         log["total_tokens"] = True
 
         self.assertRaisesRegex(
             TypeError,
             "log.total_tokens must be an integer",
-            send_to_db,
-            url,
+            check_log_type,
             log,
         )
 
