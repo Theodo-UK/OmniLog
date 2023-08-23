ea72d3b35fd384f43175f5d829789fef32f4a9ca
François Lorne <francois.lorne@hotmail.com>
2023-08-23 12:09:51 +0100

    style(front): add gap between table and search bar

--- front/src/atomic/atoms/CardAtom.tsx
+++ front/src/atomic/atoms/CardAtom.tsx
@@ -1,6 +1,6 @@
 export const CardAtom = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="border bg-white shadow rounded-md p-4 w-full mx-auto">
+        <div className="border bg-white shadow rounded-md p-4 w-full mx-auto flex flex-col gap-4">
             {children}
         </div>
     );
