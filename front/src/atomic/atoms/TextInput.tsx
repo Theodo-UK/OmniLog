6784c166965e5c4aa6c35c5b0e17f3fc907a04e0
François Lorne <francois.lorne@hotmail.com>
2023-08-23 12:06:41 +0100

    feat(front): create atoms for search icon and text input

---
+++ front/src/atomic/atoms/TextInput.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const TextInput = ({
+    placeholder = '',
+    icon = '',
+}: {
+    placeholder: string;
+    icon: React.ReactNode;
+}) => {
+    return (
+        <div className="relative">
+            <input
+                type="text"
+                className="w-full border-2 border-gray-300 bg-white h-10 px-4 pr-16 rounded-lg focus:border-theodo-blue focus:outline-none"
+                placeholder={placeholder}
+            />
+            <div className="absolute right-0 top-0 py-3 px-3">
+                {icon}
+            </div>
+        </div>
+    );
+}
