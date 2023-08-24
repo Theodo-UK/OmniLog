61c7151b98ae522fb3081cb24a45f11b822abd14
Justin <57725347+juskek@users.noreply.github.com>
2023-08-24 17:21:21 +0100

    refactor(sdk): update input_validation to match prisma type

--- sdk-python/omnilogger/input_validation.py
+++ sdk-python/omnilogger/input_validation.py
@@ -10,18 +10,23 @@ def check_log_type(log):
     if not isinstance(log, dict):
         raise TypeError(
             "Have you checked your log is a dictionary?\nlog must be a dictionary with"
-            " keys: datetime_utc, input, output, total_tokens"
+            " keys: datetime_utc, input_string, output_string, total_tokens"
         )
-    if set(log.keys()) != {"datetime_utc", "input", "output", "total_tokens"}:
+    if set(log.keys()) != {
+        "datetime_utc",
+        "input_string",
+        "output_string",
+        "total_tokens",
+    }:
         raise KeyError(
-            "log must be a dictionary with keys: datetime_utc, input, output,"
-            " total_tokens"
+            "log must be a dictionary with keys: datetime_utc, input_string,"
+            " output_string, total_tokens"
         )
     if not isinstance(log["datetime_utc"], datetime.datetime):
         raise TypeError("log.datetime_utc must be a datetime object")
-    if not isinstance(log["input"], str):
-        raise TypeError("log.input must be a string")
-    if not isinstance(log["output"], str):
-        raise TypeError("log.output must be a string")
+    if not isinstance(log["input_string"], str):
+        raise TypeError("log.input_string must be a string")
+    if not isinstance(log["output_string"], str):
+        raise TypeError("log.output_string must be a string")
     if type(log["total_tokens"]) is not int:
         raise TypeError("log.total_tokens must be an integer")
