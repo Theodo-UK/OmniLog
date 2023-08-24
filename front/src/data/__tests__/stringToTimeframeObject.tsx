c2362b93faae51373dd921b5d2599371fae9a0b9
François Lorne <francois.lorne@hotmail.com>
2023-08-24 18:21:00 +0100

    test(front): validate timeframe object computation

---
+++ front/src/data/__tests__/stringToTimeframeObject.tsx
@@ -0,0 +1,44 @@
+import "@testing-library/jest-dom";
+import { stringToTimeframeObject } from "../helpers/stringToTimeframeObject";
+
+describe("stringToTimeframeObject", () => {
+    it(
+        "When calling with a Last hour, " +
+            "then returns the correct timeframe",
+        () => {
+            const timestamp = stringToTimeframeObject("Last hour");
+            const difference =
+                timestamp.lte.getTime() - timestamp.gte.getTime();
+            expect(difference).toEqual(3600000);
+        },
+    );
+    it(
+        "When calling with a Last day, " + "then returns the correct timeframe",
+        () => {
+            const timestamp = stringToTimeframeObject("Last day");
+            const difference =
+                timestamp.lte.getTime() - timestamp.gte.getTime();
+            expect(difference).toEqual(86400000);
+        },
+    );
+    it(
+        "When calling with a Last week, " +
+            "then returns the correct timeframe",
+        () => {
+            const timestamp = stringToTimeframeObject("Last week");
+            const difference =
+                timestamp.lte.getTime() - timestamp.gte.getTime();
+            expect(difference).toEqual(604800000);
+        },
+    );
+    it(
+        "When calling with a unexpected string, " +
+            "then returns the timeframe corresponding to Last hour",
+        () => {
+            const timestamp = stringToTimeframeObject("unexpected string");
+            const difference =
+                timestamp.lte.getTime() - timestamp.gte.getTime();
+            expect(difference).toEqual(3600000);
+        },
+    );
+});
