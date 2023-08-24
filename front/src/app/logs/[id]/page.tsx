fda3b7cd66b11e8e829002a2139f5325bff02bd2
François Lorne <francois.lorne@hotmail.com>
2023-08-24 17:24:59 +0100

    fix(front): update file path after deletion of index

--- front/src/app/logs/[id]/page.tsx
+++ front/src/app/logs/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { CardAtom } from "@/atomic/atoms";
+import { CardAtom } from "@/atomic/atoms/CardAtom";
 import { LogsData } from "@/data/LogsData";
 
 export default async function LogDetails({
