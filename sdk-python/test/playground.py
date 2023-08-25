91d7faf98d88d3746afe6473d8e0650fb644c28f
Justin <57725347+juskek@users.noreply.github.com>
2023-08-25 09:30:06 +0100

    chore(sdk): configure playground as script

--- sdk-python/test/playground.py
+++ sdk-python/test/playground.py
@@ -5,15 +5,18 @@ from dotenv import load_dotenv
 
 from omnilogger import start_listener
 
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+if __name__ == "__main__":
+    load_dotenv()
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    if DATABASE_URL is None or OPENAI_API_KEY is None:
+        raise ValueError("Missing environment variables")
 
-start_listener(DATABASE_URL)
+    start_listener(DATABASE_URL)
 
-openai.api_key = OPENAI_API_KEY
-openai.Completion.create(
-    model="text-davinci-003",
-    prompt="How are rubber ducks used in IT",
-    temperature=0.6,
-)
+    openai.api_key = OPENAI_API_KEY
+    openai.Completion.create(
+        model="text-davinci-003",
+        prompt="is it working now",
+        temperature=0.6,
+    )
