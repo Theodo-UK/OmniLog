6bf2872eeff124e46dd7996524ecdde86e547b7c
François Lorne <francois.lorne@hotmail.com>
2023-08-29 17:04:10 +0100

    fix(front): update tailwind config to follow refactor

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -3,7 +3,8 @@ import type { Config } from "tailwindcss";
 const config: Config = {
     content: [
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/atomic/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
         extend: {
