f755057c34db731d91afea5da3db263d074137ea
François Lorne <francois.lorne@hotmail.com>
2023-08-29 15:08:01 +0100

    feat(front): enforce valid TimeOption type in TimeDropdown

--- front/src/features/filter/TimeDropdown.tsx
+++ front/src/features/filter/TimeDropdown.tsx
@@ -5,10 +5,14 @@ import { TimeOption } from "@/types/filter";
 export const TimeDropdown = () => {
     const { searchParams, updateSearchParam } = useNavigation();
     const timeOptions = ["Last hour", "Last day", "Last week"];
-    const selectedTime =
-        (searchParams.get("dateTimeFilter") as TimeOption) || "Last hour";
+    const selectedTime = searchParams.get("dateTimeFilter");
+    if (!selectedTime || !timeOptions.includes(selectedTime)) {
+        updateSearchParam({ dateTimeFilter: "Last hour" });
+        return null;
+    }
     const onTimeSelect = (newValue: string) => {
-        updateSearchParam({ dateTimeFilter: newValue as TimeOption });
+        if (timeOptions.includes(newValue))
+            updateSearchParam({ dateTimeFilter: newValue as TimeOption });
     };
     return (
         <Dropdown
