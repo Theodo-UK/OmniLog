9302d32380c70965e31d530f5b8d089eff5eadd1
François Lorne <francois.lorne@hotmail.com>
2023-08-29 11:40:37 +0100

    refactor(front): split dropdown by atoms&molecule

---
+++ front/src/atomic/atoms/DropdownButton.tsx
@@ -0,0 +1,19 @@
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const DropdownButton = ({
+    text,
+    faIcon,
+}: {
+    text: string;
+    faIcon: IconDefinition;
+}) => {
+    return (
+        <>
+            <span className="block truncate text-base">{text}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 text-theodo-dark-blue">
+                <FontAwesomeIcon icon={faIcon} />
+            </span>
+        </>
+    );
+};
---
+++ front/src/atomic/atoms/DropdownOptionTile.tsx
@@ -0,0 +1,27 @@
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const DropdownOptionTile = ({
+    option,
+    isSelected,
+}: {
+    option: string;
+    isSelected: boolean;
+}) => {
+    return (
+        <>
+            <span
+                className={`block truncate ${
+                    isSelected ? "font-medium" : "font-normal"
+                }`}
+            >
+                {option}
+            </span>
+            {isSelected ? (
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-theodo-dark-blue">
+                    <FontAwesomeIcon icon={faCheck} />
+                </span>
+            ) : null}
+        </>
+    );
+};
