19ba5886e6316d0ca5f48b61a4c02632dd641569
François Lorne <francois.lorne@hotmail.com>
2023-08-31 17:16:51 +0100

    test(front): check the searchCondition formatter

---
+++ front/src/services/__tests__/stringToSearchCondition.ts
@@ -0,0 +1,18 @@
+import "@testing-library/jest-dom";
+import { stringToSearchCondition } from "../helpers/formatSearchParamToPrismaQuery";
+
+describe("stringToSearchCondition", () => {
+    it("should return an array searching if input or output contains the string argument", () => {
+        const searchString = "test";
+        const expected = [
+            {
+                input_string: { contains: searchString },
+            },
+            {
+                output_string: { contains: searchString },
+            },
+        ];
+
+        expect(stringToSearchCondition(searchString)).toEqual(expected);
+    });
+});
