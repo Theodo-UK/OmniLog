c5bf3ce41429706dfe60e7d9e43574d328ed6713
Justin <57725347+juskek@users.noreply.github.com>
2023-08-31 15:59:33 +0100

    chore(app): cleanup

--- front/src/app/api/auth/[...nextauth]/route.ts
+++ front/src/app/api/auth/[...nextauth]/route.ts
@@ -46,36 +46,10 @@ export const authOptions: NextAuthOptions = {
                     id: user.id + "",
                     email: user.email,
                     name: user.name,
-                    randomKey: "Hey cool",
                 };
             },
         }),
     ],
-    //   callbacks: {
-    //     session: ({ session, token }) => {
-    //       console.log('Session Callback', { session, token })
-    //       return {
-    //         ...session,
-    //         user: {
-    //           ...session.user,
-    //           id: token.id,
-    //           randomKey: token.randomKey
-    //         }
-    //       }
-    //     },
-    //     jwt: ({ token, user }) => {
-    //       console.log('JWT Callback', { token, user })
-    //       if (user) {
-    //         const u = user as unknown as any
-    //         return {
-    //           ...token,
-    //           id: u.id,
-    //           randomKey: u.randomKey
-    //         }
-    //       }
-    //       return token
-    //     }
-    //   }
 };
 
 const handler = NextAuth(authOptions);
