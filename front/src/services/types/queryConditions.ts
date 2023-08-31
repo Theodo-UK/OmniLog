0df41e2fad826ae213aaba6bc8c135c18332d35f
François Lorne <francois.lorne@hotmail.com>
2023-08-31 16:51:32 +0100

    feat(front): enable log searching from url param

--- front/src/services/types/queryConditions.ts
+++ front/src/services/types/queryConditions.ts
@@ -9,3 +9,12 @@ export type PrismaSort =
     | { id: Order }
     | { datetime_utc: Order }
     | { total_tokens: Order };
+
+export type SearchCondition = [
+    {
+        input_string: { contains: string };
+    },
+    {
+        output_string: { contains: string };
+    },
+];
