6205283d65b5755196a0e5695ac4be9aed1c33a3
François Lorne <francois.lorne@hotmail.com>
2023-09-01 11:43:33 +0100

    test(front): update test for extractDataFromSearchParam

--- front/src/services/__tests__/extractDataFromSearchParam.ts
+++ front/src/services/__tests__/extractDataFromSearchParam.ts
@@ -2,15 +2,48 @@ import "@testing-library/jest-dom";
 import { extractDataFromSearchParam } from "../helpers/formatSearchParamToPrismaQuery";
 
 describe("extractDataFromSearchParam", () => {
-    it("should return an object with the key as the property and the order as the value", () => {
-        const searchParamsString = new URLSearchParams(
-            "sortBy=datetime_utc&sortOrder=asc",
-        );
-        const { timeframe, sort } =
-            extractDataFromSearchParam(searchParamsString);
+    it("should return an object with the default values if no search params are passed", () => {
+        const { timeframe, sort, searchCondition } =
+            extractDataFromSearchParam();
 
         const difference = timeframe.lte.getTime() - timeframe.gte.getTime();
         expect(difference).toEqual(3600000);
-        expect(sort).toEqual({ datetime_utc: "asc" });
+        expect(sort).toEqual({ datetime_utc: "desc" });
+        expect(searchCondition).toEqual([
+            {
+                input_string: {
+                    contains: "",
+                },
+            },
+            {
+                output_string: {
+                    contains: "",
+                },
+            },
+        ]);
+    });
+
+    it("should return three non-default objects if parameters are defined", () => {
+        const searchParamsString = new URLSearchParams(
+            "sortBy=total_tokens&sortOrder=asc&search=first&dateTimeFilter=Last+week",
+        );
+        const { timeframe, sort, searchCondition } =
+            extractDataFromSearchParam(searchParamsString);
+
+        const difference = timeframe.lte.getTime() - timeframe.gte.getTime();
+        expect(difference).toEqual(604800000);
+        expect(sort).toEqual({ total_tokens: "asc" });
+        expect(searchCondition).toEqual([
+            {
+                input_string: {
+                    contains: "first",
+                },
+            },
+            {
+                output_string: {
+                    contains: "first",
+                },
+            },
+        ]);
     });
 });
