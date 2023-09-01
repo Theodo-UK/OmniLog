d95fbae077256fe8993f51bd603e4b8e07d64e79
Justin <57725347+juskek@users.noreply.github.com>
2023-09-01 11:52:36 +0100

    chore(app): add docs on excluding packages from client for security

--- front/next.config.js
+++ front/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
     experimental: {
         typedRoutes: true,
+        // serverComponentsExternalPackages lists packages that should not be called by the client (see https://youtu.be/2kgqPvs0j_I?t=1285)
         serverComponentsExternalPackages: ['@prisma/client','bcryptjs']
     },
 };
