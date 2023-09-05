9a56d76442d989aa19258db7ecbb7436c2537011
Justin <57725347+juskek@users.noreply.github.com>
2023-09-05 15:20:19 +0100

    feat: add email validation to create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -3,10 +3,20 @@
 echo "Enter details of user to be created."
 
 read -rp "Enter user name: " name
-read -rp "Enter user email: " email
+
+# Email validation loop
+while true; do
+    read -rp "Enter user email: " email
+    if [[ "$email" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$ ]]; then
+        break
+    else
+        echo "Error: Invalid email format. Please enter a valid email address."
+    fi
+done
+
 read -rp "Enter user password: " password
 
-# Input validation
+# Check all values are provided
 if [ -z "$name" ] || [ -z "$email" ] || [ -z "$password" ]; then
     echo "Error: Please provide valid inputs for all fields."
     exit 1
