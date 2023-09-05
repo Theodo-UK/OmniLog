3d78163019726ddba62bc0f502f2d4400ec0f66a
François Lorne <francois.lorne@hotmail.com>
2023-09-05 12:03:57 +0100

    refactor(sdk): use openai decorator to send logs

--- sdk-python/omnilogger/openai_listener.py
+++ sdk-python/omnilogger/openai_listener.py
@@ -1,11 +1,21 @@
+from datetime import datetime
+from .database import send_to_db
 import openai
+from prisma.types import llm_logsCreateInput
 
 
 class CustomCompletion(openai.Completion):
     @classmethod
     def create(cls, model: str, prompt: str, **kwargs):
         result = super().create(model=model, prompt=prompt, **kwargs)
-        print(prompt, result)  # TODO: reformat and call send_to_db
+        log = llm_logsCreateInput(
+            input_string=prompt,
+            output_string=result["choices"][0]["text"],
+            datetime_utc=datetime.utcfromtimestamp(result["created"]),
+            total_tokens=result["usage"]["total_tokens"],
+        )
+
+        send_to_db(log)
         return result
 
 
