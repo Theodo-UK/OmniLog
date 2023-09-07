96abe19d313440ba121aa680f34775d805a71aee
Justin <57725347+juskek@users.noreply.github.com>
2023-09-07 16:18:39 +0100

    refactor(build): move error handling to function

--- front/scripts/yarn_deploy.sh
+++ front/scripts/yarn_deploy.sh
@@ -7,7 +7,7 @@ script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
 cd "$script_path" || exit 1
 
 source ./source_if_file_exists.sh
-
+source ./handle_errors.sh
 source ./check_var.sh
 
 source_if_file_exists "../.env.production"
@@ -30,16 +30,5 @@ yarn generate
 echo "Running yarn sst deploy..."
 # try catch 
 if ! error_output=$(yarn sst deploy --profile "$AWS_PROFILE_NAME" --stage "$SST_STAGE_NAME" 2>&1 1>/dev/tty); then
-    echo "$error_output"
-    echo "================================"
-    # Check if the error message in the error_output contains the string indicating it's a credentials issue
-    if [[ $error_output == *"Could not load credentials from any providers"* ]]; then
-        echo "sst deploy failed because credentials from your AWS profile could not be loaded. Have you checked the following?
-        - Does the AWS profile you specified exist? ($AWS_PROFILE_NAME)
-        - If the AWS profile has MFA, has it been authorised (e.g. using Leapp: https://www.leapp.cloud/)"
-    else
-        echo "Something went wrong."
-    fi
-    echo "================================"
-    exit 1
+    handle_errors "$error_output"
 fi
