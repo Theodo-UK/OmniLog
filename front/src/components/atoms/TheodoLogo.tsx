b207e8cca153133b5d15991cf27d4eb3c62247c8
François Lorne <francois.lorne@hotmail.com>
2023-09-08 17:41:05 +0100

    style(front): add text to logos

--- front/src/components/atoms/TheodoLogo.tsx
+++ front/src/components/atoms/TheodoLogo.tsx
@@ -3,19 +3,24 @@ import Image from "next/image";
 
 export const TheodoLogo = () => {
     return (
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-start justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <p
+                className="text-lg text-center -mr-8 mt-8"
+            >
+                by
+            </p>
+
             <a
                 className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
                 href="https://www.theodo.co.uk/"
                 target="_blank"
                 rel="noopener noreferrer"
             >
-
                 <Image
                     src={logo.src}
                     alt="Theodo logo"
-                    height={120}
-                    width={120}
+                    height={150}
+                    width={150}
                 />
             </a>
         </div>
