75c9ce885aceadf64b3b4b0c6c0d7199fad56bbc
Justin <57725347+juskek@users.noreply.github.com>
2023-09-08 14:41:38 +0100

    build(init): check for env files before prompting for config

--- init.sh
+++ init.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 source ./scripts/create_env_file.sh
+source ./scripts/configure_env_files.sh
 
 cd front || exit 1
 
-read -rp "Enter your database URI (e.g. postgres://yourname:yourpassword@your-database.domain/path): " DATABASE_URI
-
-read -rp "Enter your AWS profile name (see https://github.com/Theodo-UK/OmniLog/blob/main/docs/aws_setup.md if you have not set this up): " AWS_PROFILE_NAME
-read -rp "Enter the AWS region you would like to deploy to: " AWS_REGION
-read -rp "Enter a name to identify omnilog resources on your AWS (default: omnilog): " SST_STAGE_NAME
-SST_STAGE_NAME=${SST_STAGE_NAME:-omnilog} # set SST_STAGE_NAME to omnilog if unset or empty
-read -rp "Enter NEXTAUTH_SECRET (e.g. using openssl rand -hex 32): " NEXTAUTH_SECRET
-read -rp "Enter the target URI where the omnilog web app will deploy: " NEXTAUTH_URL
-
-
-# Create .env files
-create_env_file ".env" "$DATABASE_URI"
-create_env_file ".env.development" "$DATABASE_URI" "$AWS_PROFILE_NAME" "$AWS_REGION" "$SST_STAGE_NAME"
-create_env_file ".env.production" "$DATABASE_URI" "$AWS_PROFILE_NAME" "$AWS_REGION" "$SST_STAGE_NAME" "$NEXTAUTH_SECRET" "$NEXTAUTH_URL"
+if [[ -e .env || -e .env.development || -e .env.production ]]; then
+  read -rp "Environment files exist. Do you want to reconfigure them? (y/N): " RECONFIGURE
+  if [[ $RECONFIGURE =~ ^[Yy]$ ]]; then
+    configure_env_files
+  else
+    echo "Skipping reconfiguration."
+  fi
+else
+  configure_env_files
+fi
 
 yarn
 
