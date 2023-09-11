b00f1313baad47ce0ad0e8fb41cad90e666369a2
François Lorne <francois.lorne@hotmail.com>
2023-09-11 16:33:33 +0100

    refactor(front): improve test readability

--- front/src/services/__tests__/intervalToTimeframeObject.ts
+++ front/src/services/__tests__/intervalToTimeframeObject.ts
@@ -2,31 +2,34 @@ import "@testing-library/jest-dom";
 import { intervalToTimeframeObject } from "../helpers/formatSearchParamToPrismaQuery";
 import { MS_PER_HOUR } from "../helpers/timeConstants";
 
-
 describe("intervalToTimeframeObject", () => {
+    const firstDateString = "2021-01-01";
+    const secondDateString = "2021-01-02";
+    const invalidDateString = "invalid";
+
     it(
         "When calling with two valid strings, " +
         "then returns the correct timeframe",
         () => {
-            const timestamp = intervalToTimeframeObject("2021-01-01", "2021-01-02");
-            expect(timestamp.gte.getTime()).toEqual(new Date("2021-01-01").getTime());
-            expect(timestamp.lte.getTime()).toEqual(new Date("2021-01-02").getTime());
+            const timestamp = intervalToTimeframeObject(firstDateString, secondDateString);
+            expect(timestamp.gte.getTime()).toEqual(new Date(firstDateString).getTime());
+            expect(timestamp.lte.getTime()).toEqual(new Date(secondDateString).getTime());
         },
     );
     it(
         "When calling with inverted dates, " +
         "then returns the corrected timeframe",
         () => {
-            const timestamp = intervalToTimeframeObject("2021-01-02", "2021-01-01");
-            expect(timestamp.gte.getTime()).toEqual(new Date("2021-01-01").getTime());
-            expect(timestamp.lte.getTime()).toEqual(new Date("2021-01-02").getTime());
+            const timestamp = intervalToTimeframeObject(secondDateString, firstDateString);
+            expect(timestamp.gte.getTime()).toEqual(new Date(firstDateString).getTime());
+            expect(timestamp.lte.getTime()).toEqual(new Date(secondDateString).getTime());
         },
     );
     it(
         "When calling with an invalid date string, " +
         "then returns the timeframe of the last hour",
         () => {
-            const timestamp = intervalToTimeframeObject("2021-01-02", "invalid");
+            const timestamp = intervalToTimeframeObject(secondDateString, invalidDateString);
             const difference = timestamp.lte.getTime() - timestamp.gte.getTime();
             expect(difference).toEqual(MS_PER_HOUR);
         },
