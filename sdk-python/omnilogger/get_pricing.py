0b112bf80cc1e2d7b785f30b0e0b0fccad7d3c29
François Lorne <francois.lorne@hotmail.com>
2023-09-19 15:00:43 +0100

    feat(sdk): manage pricing json

---
+++ sdk-python/omnilogger/get_pricing.py
@@ -0,0 +1,35 @@
+import os
+import json
+import platformdirs
+
+
+def load_llm_pricing():
+    app_name = "omnilogger"
+    app_author = "theodo_uk"
+
+    config_dir = platformdirs.user_config_dir(app_name, app_author)
+    pricing_path = os.path.join(config_dir, "pricing.json")
+
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+
+    if not os.path.exists(pricing_path):
+        write_default_json_to(pricing_path)
+
+    pricing = read_json_at(pricing_path)
+
+    return pricing
+
+
+def read_json_at(path: str) -> dict:
+    with open(path, "r") as json_file:
+        return json.load(json_file)
+
+
+def write_default_json_to(target_path: str) -> None:
+    omnilogger_path = os.path.dirname(__file__)
+    default_json_path = os.path.join(omnilogger_path, "data/pricing.json")
+    default_json_content = read_json_at(default_json_path)
+
+    with open(target_path, "w") as json_file:
+        json.dump(default_json_content, json_file, indent=4)
