f4d601293154318ed9cad890e8ea5534a917dc5a
François Lorne <francois.lorne@hotmail.com>
2023-09-19 15:33:31 +0100

    test(sdk): test pricing getter

--- sdk-python/omnilogger/__init__.py
+++ sdk-python/omnilogger/__init__.py
@@ -1,4 +1,5 @@
 from .database import send_to_db
 from .openai_listener import start_openai_listener
+from .get_pricing import get_pricing
 
-__all__ = ["send_to_db", "start_openai_listener"]
+__all__ = ["send_to_db", "start_openai_listener", "get_pricing"]
