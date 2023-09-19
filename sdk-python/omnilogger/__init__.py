908dcb7202bbdec93987f453e2500c69e6898b6e
François Lorne <francois.lorne@hotmail.com>
2023-09-19 16:29:12 +0100

    refactor(sdk): reorder imports

--- sdk-python/omnilogger/__init__.py
+++ sdk-python/omnilogger/__init__.py
@@ -1,5 +1,5 @@
-from .database import send_to_db
 from .calculator import cost_calculator
+from .database import send_to_db
 from .openai_listener import start_openai_listener
 
 __all__ = ["send_to_db", "start_openai_listener", "cost_calculator"]
