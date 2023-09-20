df771ed3b60bf705305bb8e46e59d9bbf21dd92f
Justin <57725347+juskek@users.noreply.github.com>
2023-09-20 14:27:44 +0100

    ci(sdk): fix linting errors in openai_listener

--- sdk-python/omnilogger/openai_listener.py
+++ sdk-python/omnilogger/openai_listener.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Any
 
 import openai
 from prisma.types import llm_logsCreateInput
@@ -8,10 +9,12 @@ from .database import send_to_db
 
 class CustomCompletion(openai.Completion):
     @classmethod
-    def create(cls, model: str, prompt: str, **kwargs):
-        result = super().create(model=model, prompt=prompt, **kwargs)
+    def create(cls, *args: Any, **kwargs: Any):
+        result: dict[str, Any] = super().create(*args, **kwargs)  # type: ignore
+        if "prompt" not in kwargs:
+            raise ValueError("**kwargs 'prompt' is required")
         log = llm_logsCreateInput(
-            input_string=prompt,
+            input_string=kwargs["prompt"],
             output_string=result["choices"][0]["text"],
             datetime_utc=datetime.utcfromtimestamp(result["created"]),
             total_tokens=result["usage"]["total_tokens"],
