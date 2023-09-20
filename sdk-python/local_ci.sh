f7abae03ac75245fb2984c5621801b95c135c12b
Justin <57725347+juskek@users.noreply.github.com>
2023-09-20 12:08:18 +0100

    fix: linting errors for local_ci.sh

--- sdk-python/local_ci.sh
+++ sdk-python/local_ci.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 poetry run isort omnilogger test --check-only
 poetry run flake8
-poetry run pylint */**.py --errors-only
+poetry run pylint ./*/**.py --errors-only
 poetry run pytest
 poetry run python test/openai_api/validate_format.py
