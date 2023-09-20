a095f037a36c3c7e32d9279dcf4fbb4ff751909c
Justin <57725347+juskek@users.noreply.github.com>
2023-09-20 14:47:57 +0100

    ci(sdk): fix linting errors in input_validation

--- sdk-python/omnilogger/input_validation.py
+++ sdk-python/omnilogger/input_validation.py
@@ -5,7 +5,6 @@ from prisma.types import llmLogsCreateInput
 from .errors import LogDictKeyError
 
 
-
 def check_log_type(log: llmLogsCreateInput):
     assert isinstance(log, dict), LogDictKeyError()
     assert set({**log.copy(), "id": -1}.keys()) == set(
@@ -24,4 +23,5 @@ def check_log_type(log: llmLogsCreateInput):
     assert type(log["total_tokens"]) is int, TypeError(
         "log.total_tokens must be an integer"
     )
-    assert isinstance(log["cost"], float), TypeError("log.cost must be a float")
+    if "cost" in log:
+        assert isinstance(log["cost"], float), TypeError("log.cost must be a float")
