609b98330919a77110b8255cb54000dea4c7f683
François Lorne <francois.lorne@hotmail.com>
2023-09-20 16:59:51 +0100

    feat(front): create cost formatter with TDD

---
+++ front/src/features/tables/helpers/__tests__/formatCostToString.ts
@@ -0,0 +1,48 @@
+import "@testing-library/jest-dom";
+import { formatCostToString } from "../formatCost";
+
+describe("formatCostToString", () => {
+    it("should return a string with the cost in USD with cents", () => {
+        const cost = 100;
+        const costString = formatCostToString(cost);
+        expect(costString).toEqual("100.00");
+    }
+    );
+    it("should return 0.00 if the cost is 0", () => {
+        const cost = 0;
+        const costString = formatCostToString(cost);
+        expect(costString).toEqual("0.00");
+    }
+    );
+    it("should keep at least two decimals", () => {
+        const cost = 1;
+        const costString = formatCostToString(cost);
+        expect(costString).toEqual("1.00");
+    }
+    );
+    it("should write thousands with a k", () => {
+        const cost = 1000;
+        const costString = formatCostToString(cost);
+        expect(costString).toEqual("1.00k");
+    }
+    );
+    it("should round number to the nearest 0.0001", () => {
+        const cost = 1.00004;
+        const costString = formatCostToString(cost);
+        expect(costString).toEqual("1.00");
+    }
+    );
+    it("should add a star if the cost is rounded to zero", () => {
+        const cost = 0.000004;
+        const costString = formatCostToString(cost);
+        expect(costString).toEqual("0.00*");
+    }
+    );
+    it("should return a dash if the cost is undefined", () => {
+        const cost = undefined;
+        const costString = formatCostToString(cost);
+        expect(costString).toEqual("-");
+    }
+    );
+}
+);
---
+++ front/src/features/tables/helpers/formatCost.ts
@@ -0,0 +1,29 @@
+export const formatCostToString = (cost: number | undefined): string => {
+    if (cost === undefined) {
+        return "-";
+    }
+    if (cost >= 1000) {
+        const thousands = cost / 1000;
+        const cleanThousandsString = formatNumber(thousands);
+        return `${cleanThousandsString}k`;
+    }
+    const cleanCostString = formatNumber(cost);
+    if (cleanCostString === "0.00" && cost !== 0) {
+        return "0.00*";
+    }
+
+    return cleanCostString;
+}
+
+const formatNumber = (num: number) => {
+    const rounded = roundNumber(num, 4);
+    const decimalsString = rounded.toString().split(".")[1];
+    const decimalsStringWithZeros = decimalsString?.padEnd(2, "0") ?? "00";
+    const integerString = rounded.toString().split(".")[0] ?? "0";
+    return `${integerString}.${decimalsStringWithZeros}`;
+}
+
+const roundNumber = (num: number, decimals: number) => {
+    const rounded = Math.round(num * Math.pow(10, decimals)) / Math.pow(10, decimals);
+    return rounded;
+}
