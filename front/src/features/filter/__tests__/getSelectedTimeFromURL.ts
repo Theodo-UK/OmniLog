10bf1044dfb98dd47896cfbdc05d7479f74e1ee8
François Lorne <francois.lorne@hotmail.com>
2023-10-03 15:37:17 +0100

    test(front): add dateTimeFilter options to selectedTime init test

--- front/src/features/filter/__tests__/getSelectedTimeFromURL.ts
+++ front/src/features/filter/__tests__/getSelectedTimeFromURL.ts
@@ -13,15 +13,48 @@ describe("getSelectedTimeFromURL", () => {
         );
         expect(result).toEqual("Custom interval");
     });
-    it("should return 'Last hour' if dateTimeFilter is not valid", () => {
-        const startDateTime = "";
-        const endDateTime = "";
-        const dateTimeFilter = "this is not valid";
+    it("should return 'Last hour' if dateTimeFilter is not defined", () => {
+        const startDateTime = undefined;
+        const endDateTime = undefined;
+        const dateTimeFilter = undefined;
         const result = getSelectedTimeFromURL(
             startDateTime,
             endDateTime,
             dateTimeFilter,
         );
         expect(result).toEqual("Last hour");
+    });
+    it("should return 'Last hour' if dateTimeFilter is Last hour", () => {
+        const startDateTime = undefined;
+        const endDateTime = undefined;
+        const dateTimeFilter = "Last hour";
+        const result = getSelectedTimeFromURL(
+            startDateTime,
+            endDateTime,
+            dateTimeFilter,
+        );
+        expect(result).toEqual("Last hour");
+    });
+    it("should return 'Last day' if dateTimeFilter is Last day", () => {
+        const startDateTime = undefined;
+        const endDateTime = undefined;
+        const dateTimeFilter = "Last day";
+        const result = getSelectedTimeFromURL(
+            startDateTime,
+            endDateTime,
+            dateTimeFilter,
+        );
+        expect(result).toEqual("Last day");
+    });
+    it("should return 'Last week' if dateTimeFilter is Last week", () => {
+        const startDateTime = undefined;
+        const endDateTime = undefined;
+        const dateTimeFilter = "Last week";
+        const result = getSelectedTimeFromURL(
+            startDateTime,
+            endDateTime,
+            dateTimeFilter,
+        );
+        expect(result).toEqual("Last week");
     });
 });
