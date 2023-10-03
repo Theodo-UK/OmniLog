ca0cac21b9d6880c3ee968691baee018012a0d53
Claire Squires <claires@theodo.co.uk>
2023-10-03 17:25:09 +0100

    refactor (front): improve names for TimeOptionConstantType

--- front/src/services/helpers/timeConstants.ts
+++ front/src/services/helpers/timeConstants.ts
@@ -5,25 +5,25 @@ export const MS_PER_DAY = 24 * MS_PER_HOUR;
 export const MS_PER_WEEK = 7 * MS_PER_DAY;
 
 type TimeOptionConstantType = {
+    timeOption: TimeOption;
     displayName: string;
-    urlName: TimeOption;
     duration: number;
 };
 
 export const timeOptionConstants: TimeOptionConstantType[] = [
     {
+        timeOption: "last-hour",
         displayName: "Last hour",
-        urlName: "last-hour",
         duration: MS_PER_HOUR,
     },
     {
+        timeOption: "last-day",
         displayName: "Last day",
-        urlName: "last-day",
         duration: MS_PER_DAY,
     },
     {
+        timeOption: "last-week",
         displayName: "Last week",
-        urlName: "last-week",
         duration: MS_PER_WEEK,
     },
 ];
