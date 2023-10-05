0fe0b955e7b4d096cf5615629199c934c1b979a2
Guillaume Barra <guillaume.barra2@gmail.com>
2023-10-05 14:12:53 +0100

    fix: logo text

--- front/src/components/atoms/OmnilogLogo.tsx
+++ front/src/components/atoms/OmnilogLogo.tsx
@@ -5,7 +5,7 @@ export const OmnilogLogo = () => {
     return (
         <div className="justify-center m-auto">
             <Image src={logo.src} alt="Omnilog logo" height={150} width={150} />
-            <p className="text-2xl font-bold text-center -m-4 drop-shadow-[0px_-2px_0px_rgba(250,250,255,1)]">
+            <p className="text-2xl dark:text-white font-bold text-center -m-4 drop-shadow-[0px_-2px_0px_rgba(250,250,255,1)]">
                 Omnilog
             </p>
         </div>
