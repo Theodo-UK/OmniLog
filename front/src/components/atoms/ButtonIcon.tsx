29d497b9cea68bed8e129bc3b2e788f4350552b3
François Lorne <francois.lorne@hotmail.com>
2023-10-06 11:53:44 +0100

    feat(front): create button atom with icon

---
+++ front/src/components/atoms/ButtonIcon.tsx
@@ -0,0 +1,26 @@
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { MouseEventHandler } from "react";
+
+export const ButtonIcon = ({
+    onClick,
+    icon,
+}: {
+    onClick: () => void;
+    icon: IconDefinition;
+}) => {
+    const onProtectedclick: MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.stopPropagation();
+        onClick();
+    };
+
+    return (
+        <button
+            className={` bg-white hover:bg-theodo-grey-regular outline outline-theodo-grey-regular text-gray-600 rounded-3xl h-6 w-6 justify-center align-middle transition-colors duration-100`}
+            onClick={onProtectedclick}
+        >
+            <FontAwesomeIcon icon={icon} />
+        </button>
+    );
+};
