bacbf0329ce20b7b093de99b4ad16cb5ce15d3d2
François Lorne <francois.lorne@hotmail.com>
2023-10-09 11:07:17 +0100

    feat(front): enhance api error handling

--- front/src/app/api/tags/route.ts
+++ front/src/app/api/tags/route.ts
@@ -1,11 +1,18 @@
+import { getErrorMessage } from "@/app/helpers/getErrorMessage";
 import { TagsData } from "@/services/prisma/TagsData";
 import { NextResponse } from "next/server";
 
 export const GET = async (): Promise<NextResponse> => {
     try {
         const tags = await TagsData.getTags();
-        return NextResponse.json({ tags });
-    } catch (err) {
-        return NextResponse.json({ error: "failed to fetch tags from prisma" });
+        return NextResponse.json({ tags }, { status: 200 });
+    } catch (err: unknown) {
+        const message = getErrorMessage(err);
+        return NextResponse.json(
+            {
+                error: `failed to fetch tags from prisma: ${message}`,
+            },
+            { status: 500 },
+        );
     }
 };
