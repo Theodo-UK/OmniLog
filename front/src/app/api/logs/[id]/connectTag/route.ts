23c53c86bad523fe3aca855a8f3065977f04343b
Claire Squires <claires@theodo.co.uk>
2023-10-09 17:22:57 +0100

    refactor: catch more detailed prisma errors

--- front/src/app/api/logs/[id]/connectTag/route.ts
+++ front/src/app/api/logs/[id]/connectTag/route.ts
@@ -1,4 +1,6 @@
+import { prismaErrorHandler } from "@/features/tags/utils";
 import { LogsData } from "@/services/LogsData";
+import { Prisma } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 type QueryParams = {
@@ -6,11 +8,25 @@ type QueryParams = {
 };
 
 export async function POST(req: Request, { params }: QueryParams) {
+    let json;
+    let tagId;
+
     try {
-        const json = await req.json();
-        const { tagId } = json;
+        json = await req.json();
+        ({ tagId } = json);
+    } catch (error) {
+        return NextResponse.json(
+            { error: "No tagId found in the body of the post request" },
+            { status: 400 },
+        );
+    }
+
+    try {
         await LogsData.connectTagToLog(params.id, tagId);
     } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            return prismaErrorHandler(error);
+        }
         return NextResponse.json({ error: error }, { status: 500 });
     }
     return NextResponse.json({}, { status: 201 });
