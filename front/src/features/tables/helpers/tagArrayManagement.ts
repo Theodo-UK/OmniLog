56006edeb79fdf082e460798f2d73092c510f477
François Lorne <francois.lorne@hotmail.com>
2023-10-11 10:51:09 +0100

    refactor(front): use built-in string comparison

--- front/src/features/tables/helpers/tagArrayManagement.ts
+++ front/src/features/tables/helpers/tagArrayManagement.ts
@@ -17,14 +17,6 @@ export const findTagById = (tags: Tag[], tagId: string): Tag => {
 export const concatAndSortTags = (tags: Tag[], newTag: Tag): Tag[] => {
     const newTagArray = [...tags, newTag];
     return newTagArray.sort((tagA, tagB) => {
-        return compareStrings(tagA.name, tagB.name);
+        return tagA.name.localeCompare(tagB.name);
     });
-};
-
-const compareStrings = (stringA: string, stringB: string): number => {
-    const stringALowerCase = stringA.toLowerCase();
-    const stringBLowerCase = stringB.toLowerCase();
-    if (stringALowerCase < stringBLowerCase) return -1;
-    if (stringBLowerCase < stringALowerCase) return 1;
-    return 0;
 };
