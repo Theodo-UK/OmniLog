080458327380f69789fb8fa48208f57aa718e520
François Lorne <francois.lorne@hotmail.com>
2023-10-17 15:25:19 +0100

    test(front): check logData converters' behaviour

---
+++ front/src/services/__tests__/convertToLogData.ts
@@ -0,0 +1,60 @@
+import { ProjectData, TagData } from "@/types/logDisplayOptions";
+import { LogDataFromPrisma, convertToLogData } from "../helpers/convertToLogData";
+import { ProjectsData } from "../prisma/ProjectsData";
+
+describe("convertToLogData", () => {
+    const tag: TagData = {
+        id: "tag-a",
+        name: "Tag A",
+    };
+    const project: ProjectData = {
+        id: "project-1",
+        name: "test",
+    };
+    let defaultProject: ProjectData;
+
+    beforeAll(() => {
+        defaultProject = ProjectsData.getDefaultProject();
+    });
+
+    it("should convert log from prisma format to logData", () => {
+        const log: LogDataFromPrisma = {
+            id: "1",
+            datetime_utc: new Date(),
+            input_string: "test",
+            output_string: "test",
+            total_tokens: 1,
+            cost: 1,
+            projectId: "project-1",
+            project,
+            tags: [tag],
+        };
+
+        const result = convertToLogData(log);
+
+        expect(result).toEqual(log);
+    }
+    );
+    it("should add a default project to log if one is missing", () => {
+        const log: LogDataFromPrisma = {
+            id: "1",
+            datetime_utc: new Date(),
+            input_string: "test",
+            output_string: "test",
+            total_tokens: 1,
+            cost: 1,
+            projectId: null,
+            project: null,
+            tags: [tag],
+        };
+
+        const result = convertToLogData(log);
+
+        expect(result).toEqual({
+            ...log,
+            project: defaultProject,
+            projectId: defaultProject.id,
+        });
+    }
+    );
+});
